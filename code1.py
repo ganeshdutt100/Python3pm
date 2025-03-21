a954bfc691b45744f821c3058d4632dfc3c56786
Ganesh Dutt <gdutt2304@gmail.com>
2025-03-21 16:22:50 +0530

    first commit

---
+++ code1.py
@@ -0,0 +1,43 @@
+# print("hello world")
+
+# a = 4
+# b = 5.5
+# c = a+b
+# print(c)
+
+# str=  "Hello world "
+# print(str , type(str))
+
+
+# value = 56
+# print(str , value)
+
+
+# a = 56
+# print(a , type(a))
+
+#  str = "Value"
+
+
+# is_True =False 
+# print(is_True , type(is_True))
+
+
+# name = input("Enter your name: ")
+# print("Hello " , name)
+''' 
+value = int("10")
+number  = str(45)
+print( number , type(number))
+'''
+# print(value, type(value))
+
+"""
+value = int("10")
+number  = str(45)
+print( number , type(number))"
+"""
+
+# value = int("10")
+# number  = str(45)
+# print( number , type(number))"
