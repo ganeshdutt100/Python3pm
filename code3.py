ab8303cd1aa50d8bfa99583e9d8d71d26bcb2385
Ganesh Dutt <gdutt2304@gmail.com>
2025-03-22 16:18:40 +0530

    Control Flow Statements

--- code3.py
+++ code3.py
@@ -2,3 +2,10 @@ a = 78;
 
 a +=5     # a = a + 5;
 print(a)
+
+num1 = input("Enter your first number: ")
+num2 = input("Enter your Second number: ")
+# op = input("+ , -, * , /")
+
+result = int(num1) - int(num2)
+print(result)
---
+++ code4.py
@@ -0,0 +1,26 @@
+# value  = 2
+
+# if(value >= 0):
+#     print("Value is positive")
+# if(value >= 0):
+#     print("Value is positive")
+# else:
+#     print("Value is negative")
+
+
+# number  = int(input("Enter your number :"))
+# if number % 2 == 0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+
+marks = 98
+if(marks >= 60):
+    print("First Division")
+elif(marks >= 50):
+    print("second Division")
+elif(marks >= 40):
+    print("Third Division")
+else:
+    print("fail")
