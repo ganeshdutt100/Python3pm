df6e5a043a3cfc4601a2d7e170a6823a037d77f8
Ganesh Dutt <gdutt2304@gmail.com>
2025-03-22 15:49:20 +0530

    Operators

---
+++ code2.py
@@ -0,0 +1,31 @@
+a = 56
+b =89
+c = 78
+
+# 0 = False
+# 1 = True
+#  0 And 0  = 0
+#  0 And 1  = 0
+#  1 And 0  = 0
+#  1 And 1  = 1
+
+
+#  0 or 0  = 0
+#  0 or 1  = 1
+#  1 or 0  = 1
+#  1 or 1  = 1
+
+#  not 0 = 1
+#  not 1 = 0
+print(a > b and a > c) #false
+print(a > b and a < c) #false
+print(a < b and a < c) #true
+print(a >= b and a <= c) #false
+
+# print(a == b)  #false
+# print(a != b)  #true
+# print(a > b)  #false
+# print(a < b)  # true
+# print(a <= b)  # true
+# print(a >= b)  #false
+
