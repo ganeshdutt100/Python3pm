fab727c9176163f43d35f184b0522a2d08fe3887
Ganesh Dutt <gdutt2304@gmail.com>
2025-03-25 12:20:40 +0530

    Update Loop

---
+++ code7.py
@@ -0,0 +1,13 @@
+# for i in range(0,11):
+#     print(i)
+
+# fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# value1 = "Apple"
+# value2 = "Banana"
+# value3 = "cherry"
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+# for i in fruits:
+#     print(i)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
 
-            return "Tuesday"
+# value1 = "Apple"
